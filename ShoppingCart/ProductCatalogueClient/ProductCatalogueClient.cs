70ad03ee329bc72d4b50f36d3c75b80de6f5c427
vynnyk92 <ynov07@rambler.ru>
2019-06-11 23:36:23 +0300

    Product catalog start

---
+++ ShoppingCart/ProductCatalogueClient/ProductCatalogueClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShoppingCart.ShopingCart;
+
+namespace ShoppingCart.ProductCatalogueClient
+{
+    public class ProductCatalogueClient : IProductCatalogClient
+    {
+        private static string productCatalogueBaseUrl =
+     @"http://private-05cc8-chapter2productcataloguemicroservice.apiary-mock.com";
+        private static string getProductPathTemplate =
+          "/products?productIds=[{0}]";
+
+        public Task<IEnumerable<ShoppingCartItem>> GetShopingCartItems(IEnumerable<int> productCatalogueIds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
